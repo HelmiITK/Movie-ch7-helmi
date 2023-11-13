ef61e3990cc04992d18c18bcb6490c958500c54b
HelmiITK <helmi220602@gmail.com>
2023-11-13 20:45:08 +0800

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,6 @@ import thunk from "redux-thunk"
 // create the store or temporary state database
 export default configureStore({
     reducer: rootReducers,
-    devTools: import.meta.env.MODE !== "production",
+    devTools: import.meta.env.VITE_MODE !== "production",
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk), // redux thunk
 });
